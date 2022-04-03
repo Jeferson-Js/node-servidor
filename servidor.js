f6a284f008233dfe89a325842d3dca02f3640ff9
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-04-03 11:57:53 -0300

    Meu primeiro servidor em node.js

--- servidor.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-http.createServer((request, response)=>{
-response.writeHead(200,{'Content-type':'text/plain'});
-response.write('Seu servido esta no ar');
-response.end();
-}).listen(1337, ()=>{console.log('working')});
-
-
